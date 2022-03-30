5a0c38c9e1039da0fdbbb3fa7feaf8355adc5bf4
Salih Guler <muhammedsalihguler@gmail.com>
2022-03-30 17:03:06 +0300

    Initial Commit

---
+++ step_01/snippet.dart
@@ -0,0 +1,8 @@
+class TestClass extends StatelessWidget {
+  const TestClass({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ step_01/solution.dart
@@ -0,0 +1,13 @@
+class TestClassSolution extends StatefulWidget {
+  const TestClassSolution({Key? key}) : super(key: key);
+
+  @override
+  State<TestClassSolution> createState() => _TestClassSolutionState();
+}
+
+class _TestClassSolutionState extends State<TestClassSolution> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
